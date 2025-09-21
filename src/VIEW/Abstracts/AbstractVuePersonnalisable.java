cd53755a4f841f0c15acbc308b618d02bc7c8196
Fabian CISNEROS BRIDON <fabiancisnerosbridon@MacBook-Pro-de-Fabian.local>
2025-09-21 15:19:31 -0400

    Création constructeur init

--- src/VIEW/Abstracts/AbstractVuePersonnalisable.java
+++ src/VIEW/Abstracts/AbstractVuePersonnalisable.java
@@ -7,4 +7,13 @@ public abstract class AbstractVuePersonnalisable extends JDialog {
 	private int attr1;
 	private int attr2;
 	private int attr3;
+	
+	public AbstractVuePersonnalisable(int attr1, int attr2, int attr3) {
+		super();
+		this.attr1 = attr1;
+		this.attr2 = attr2;
+		this.attr3 = attr3;
+	}
+	
+	
 }
