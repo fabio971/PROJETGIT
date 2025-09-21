b726553d07efb4cc8bd1f8261068f7288a495ced
Fabian CISNEROS BRIDON <fabiancisnerosbridon@MacBook-Pro-de-Fabian.local>
2025-09-21 15:21:35 -0400

    Création getters/setters

--- src/VIEW/Abstracts/AbstractVuePersonnalisable.java
+++ src/VIEW/Abstracts/AbstractVuePersonnalisable.java
@@ -14,6 +14,29 @@ public abstract class AbstractVuePersonnalisable extends JDialog {
 		this.attr2 = attr2;
 		this.attr3 = attr3;
 	}
-	
+
+	public int getAttr1() {
+		return attr1;
+	}
+
+	public void setAttr1(int attr1) {
+		this.attr1 = attr1;
+	}
+
+	public int getAttr2() {
+		return attr2;
+	}
+
+	public void setAttr2(int attr2) {
+		this.attr2 = attr2;
+	}
+
+	public int getAttr3() {
+		return attr3;
+	}
+
+	public void setAttr3(int attr3) {
+		this.attr3 = attr3;
+	}
 	
 }
